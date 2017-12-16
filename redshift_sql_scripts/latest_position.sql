455598f14b2b0da6affd261adcd22d806b48ed9b
Rich Ung <ricky196@yahoo.com>
2017-12-16 11:31:41 -0800

    Adding latest position view mock file

---
+++ redshift_sql_scripts/latest_position.sql
@@ -0,0 +1,22 @@
+CREATE VIEW latest_position AS
+SELECT * FROM
+(
+  SELECT 
+    RANK() OVER (PARTITION BY vehicle_id ORDER BY t_stamp DESC) AS order_number,
+    A.vehicle_id,
+    A.t_stamp,
+    A.position_latitude,
+    A.position_longitude,
+    A.trip_id,
+    A.agency,
+    B.route_id,
+    B.service_id,
+    B.trip_headsign,
+    B.direction_id,
+    B.block_id,
+    B.shape_id,
+    C.name AS operator_name
+  FROM vehicle_position A
+  INNER JOIN data_feed_rich B ON TRIM(A.trip_id) = TRIM(B.trip_id)
+  INNER JOIN operator_list_rich C ON TRIM(A.agency) = TRIM(C.id))
+WHERE order_number = 1
