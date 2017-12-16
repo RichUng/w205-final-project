1f89ae5c62a3e31b0b6326d1acfd484ff59f1a3c
Rich Ung <ricky196@yahoo.com>
2017-12-16 15:55:33 -0800

    Adding changes to create table sql and tableau workbook

--- redshift_sql_scripts/latest_position.sql
+++ redshift_sql_scripts/latest_position.sql
@@ -17,6 +17,6 @@ SELECT * FROM
     B.shape_id,
     C.name AS operator_name
   FROM vehicle_position A
-  LEFT OUTER JOIN data_feed_rich B ON TRIM(A.trip_id) = TRIM(B.trip_id)
-  LEFT OUTER JOIN operator_list_rich C ON TRIM(A.agency) = TRIM(C.id))
+  LEFT OUTER JOIN data_feed B ON TRIM(A.trip_id) = TRIM(B.trip_id)
+  LEFT OUTER JOIN operator_list C ON TRIM(A.agency) = TRIM(C.id))
 WHERE order_number = 1
