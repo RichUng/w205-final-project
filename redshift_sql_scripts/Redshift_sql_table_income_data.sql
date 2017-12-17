f3aac712094f3d5be1863b6d7101386a8fd7cb3a
300ani <300ani@berkeley.edu>
2017-12-16 22:14:59 -0700

    Added income_data table

---
+++ redshift_sql_scripts/Redshift_sql_table_income_data.sql
@@ -0,0 +1,22 @@
+drop table if exists income_data;
+create table income_data(
+   statefips varchar(255),
+   state varchar(255),
+   zipcode varchar(255),
+   agi_stub varchar(255),
+   n1 varchar(255),
+   mars1 varchar(255),
+   mars2 varchar(255),
+   mars4 varchar(255),
+   prep varchar(255),
+   n2 varchar(255),
+   numdep varchar(255),
+   total_vita varchar(255),
+   vita varchar(255),
+   tce varchar(255),
+   vita_eic varchar(255),
+   ral varchar(255),
+   rac varchar(255),
+   elderly varchar(255),
+   other_fields varchar(255)
+); 
